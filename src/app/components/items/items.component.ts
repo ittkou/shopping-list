4e645a8ee7097afbcd4490037fa94d1da1432a3a
ittkou <martufuentes1@gmail.com>
2023-06-29 01:27:57 -0300

    trabajo practico terminado

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+import { ItemService } from 'src/app/services/item.service';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items:Item[] = [];
+  total:number = 0;
+
+
+  constructor(private itemServices:ItemService) { }
+
+  ngOnInit(): void {
+
+
+    this.items = this.itemServices.getItems();
+
+    this.getTotal();
+    
+    
+  }
+  deleteItem(item:Item){
+    this.items = this.items.filter(x=> x.id !== item.id);
+    this.getTotal();
+  }
+
+  toggleItem(item:Item){
+    this.getTotal();
+  }
+
+  getTotal(){
+    this.total = this.items
+                .filter(item => !item.completed)
+                .map(item => item.quantity * item.price )
+                .reduce( (acc, item) => acc += item, 0);
+  }
+}
